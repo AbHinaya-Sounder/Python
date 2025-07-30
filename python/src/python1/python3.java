93400531f8b1556ca47e3d7f14c3ac909279bcc5
KEERTHANA <KEERTHANA@DESKTOP-HNV004O>
2025-07-30 13:49:58 +0530

    Updated code

--- python/src/python1/python3.java
+++ python/src/python1/python3.java
@@ -3,9 +3,8 @@ package python1;
 public class python3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("AbhinayaS");
-
+		System.out.println("Elakiya");
 	}
 
 }
